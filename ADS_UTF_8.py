16d1577c49162a87db84292022fd1260ca8f1f43
Deividas Gedgaudas <sidicer@gmail.com>
2020-01-14 14:02:26 +0200

    Add files via upload

---
+++ ADS_UTF_8.py
@@ -0,0 +1,20 @@
+# Pabandome atsidaryti įrašymo failą kaip binarinį (ne tekstinį)
+
+with open('Recoded386intel.txt','wb') as f: # "f" - atidaryto failo kintamasis, "wb" - Write Binary Mode
+    f.write(bytes(range(256)))              # Užpildome baitais nuo 0 iki 255
+
+# Atidarius 'Recoded386intel.txt' failą matome# visus galimus simbolius nuo 0 iki 255
+#
+# Tai reiškia, kad sėkmingai įrašėme binariniu būdu,
+# todėl galime eiti prie perkodavimo:
+
+# Veiksmų seka:
+#
+# Nuskaitysime turimą '386intel.txt' failą su code page 437 kodavimu (DOS OEM-US).
+# Įrašysime į naują failą su UTF-8 kodavimu ("Unicode" nėra koduotė)
+
+with open('386intel.txt',encoding='cp437') as infile:                   # 'with' - kol dirbame su failu; 
+                                                                        # 'encoding' - nurodome kokios koduotės tikimės nuskaitydami;
+    with open('Recoded386intel.txt','w',encoding='utf8') as outfile:    # Nurodome kad išvesties failas bus utf-8 koduotės ir atidarome jį 'write' būdu
+        outfile.write(infile.read())                                    # Įrašome į išvesties failą tai ką nuskaitome įvesties faile
+# išvesties failas <- įrašyti <- įvesties failas <- nuskaityti
